bc883213b9b3feeeed252bfe51e6f4d21a4d8539
oincandenza <heythisisfake@gmail.com>
2016-11-01 16:50:41 -0700

    Update snapshotvolumeID.sh

--- snapshotvolumeID.sh
+++ snapshotvolumeID.sh
@@ -6,6 +6,6 @@
 
 SnapID=''
 
-aws ec2 describe-instances --filters "Name=tag:Snapshot,Values=$SnapID" | grep "VolumeId" 
-| awk '{print $2;}' 
-| sed 's/[\",]//g' > /home/ec2-user/ec2-mgmt/snapshot/$SnapID.txt
+aws ec2 describe-instances --filters "Name=tag:Snapshot,Values=$SnapID" |
+grep "VolumeId" | awk '{print $2;}' | 
+sed 's/[\",]//g' > /home/ec2-user/ec2-mgmt/snapshot/$SnapID.txt
