5f81367eba8035e6011cfc976aad5e3bd8eb9694
oincandenza <heythisisfake@gmail.com>
2016-11-01 16:50:17 -0700

    Create snapshotvolumeID.sh

---
+++ snapshotvolumeID.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+######
+# Fill out#
+#######
+
+SnapID=''
+
+aws ec2 describe-instances --filters "Name=tag:Snapshot,Values=$SnapID" | grep "VolumeId" 
+| awk '{print $2;}' 
+| sed 's/[\",]//g' > /home/ec2-user/ec2-mgmt/snapshot/$SnapID.txt
